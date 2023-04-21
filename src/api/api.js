ec86cfd40985886dd1a597256ad88aa709bab4ed
KrishnaManmayi <manmayi.p9@gmail.com>
2023-04-22 00:08:05 +0530

    Updated api base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 const BASE_URL =
-    "http://Recipesearchapi-env.eba-bcinprem.ap-south-1.elasticbeanstalk.com:8080";
+    "http://recipesearchapi-env.eba-bcinprem.ap-south-1.elasticbeanstalk.com";
 
 export const RecipeSearchApi = () => {
     return BASE_URL + "/api/recipe/search";
